f3adef6eb51825250da1f4db66b3de8c348c474e
Dmitry <slomov.dima@mail.ru>
2025-03-28 14:12:30 +0300

    V0.8 -Исправлены неточности в коде -Добавлен переход из гостевой истории в профиль в navigation -Реализован GuestHistory

--- app/src/main/java/com/example/kursach_handbook/ui/authorization/GuestHistory.kt
+++ app/src/main/java/com/example/kursach_handbook/ui/authorization/GuestHistory.kt
@@ -5,19 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.kursach_handbook.R
+import com.example.kursach_handbook.databinding.FragmentGuestHistoryBinding
+import com.example.kursach_handbook.databinding.FragmentGuestProfileBinding
 
 
 class GuestHistory : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentGuestHistoryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guest_history, container, false)
+        _binding = FragmentGuestHistoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.goToProfileButton.setOnClickListener {
+            findNavController().navigate(R.id.action_guestHistory_item_to_guestProfile_item)
+        }
+    }
+
 }
